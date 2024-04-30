eb9060eb7bf379c3786468e303153e5eee0019a2
Jeffrey <jeffrey@1nteger.nl>
2024-04-30 16:39:24 +0200

    remove compiled src files

--- src/client.js
+++
@@ -1,4 +0,0 @@
-import { APIClient } from "@wharfkit/antelope";
-export const client = new APIClient({
-    url: process.env.RPC_API_URL,
-});
--- src/logger.js
+++
@@ -1,6 +0,0 @@
-import { ElysiaLogging, LogFormat } from "@otherguy/elysia-logging";
-import pino from "pino";
-export const logger = pino();
-export const elysiaLogger = ElysiaLogging(logger, {
-    format: LogFormat.JSON,
-});
